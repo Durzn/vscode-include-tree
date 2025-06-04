e019649c0c7c1ec5dcd4711e8e95203854d7bf2b
Durzn <dev@durzn.com>
2025-06-04 11:58:58 +0200

    Implemented functionality of expandAll and collapseAll

--- src/TreeView/IncludeTreeDataProvider.ts
+++ src/TreeView/IncludeTreeDataProvider.ts
@@ -2,16 +2,21 @@ import * as vscode from 'vscode';
 import IncludeTree from '../IncludeTree';
 import Include from '../Include';
 import IncludeTreeItem from './IncludeTreeItem';
+import { configCache } from '../ConfigCache';
 
 export default class IncludeTreeDataProvider implements vscode.TreeDataProvider<IncludeTreeItem> {
-    constructor(private includeTree: IncludeTree | undefined = undefined) { }
+    private elements: IncludeTreeItem[] = [];
+    private expandedElements = new Set<string>();
 
-    getTreeItem(element: IncludeTreeItem): vscode.TreeItem {
+    constructor(private includeTree: IncludeTree | undefined = undefined) {
+        this.setIncludeTree(includeTree);
+    }
+
+    public getTreeItem(element: IncludeTreeItem): vscode.TreeItem {
         return element;
     }
 
-    getChildren(element?: IncludeTreeItem): Thenable<IncludeTreeItem[]> {
-        let elements: IncludeTreeItem[] = [];
+    public getChildren(element?: IncludeTreeItem): Thenable<IncludeTreeItem[]> {
         let includes: Include[] = [];
         if (element) {
             includes = element.include.includes;
@@ -20,27 +25,63 @@ export default class IncludeTreeDataProvider implements vscode.TreeDataProvider<
                 includes = this.includeTree.rootNodes;
             }
         }
-        for (let include of includes) {
-            let collapsibleState = include.includes.length === 0 ? vscode.TreeItemCollapsibleState.None : vscode.TreeItemCollapsibleState.Expanded;
-            elements.push(new IncludeTreeItem(include, include.getFileName(), {
-                'title': "Open file",
-                'command': "include-tree.open",
-                'tooltip': "Open file",
-                'arguments': [include.fileUri]
-            }, vscode.ThemeIcon.File, include.fileUri, collapsibleState));
+        const items = this.convertIncludesToIncludeTreeItems(includes);
+        return Promise.resolve(items);
+    }
+
+    public getParent(element: IncludeTreeItem): IncludeTreeItem | null {
+        if (element.include.parent) {
+            return this.elements.find(item => item.include.id === element.include.parent?.id) || null;
         }
-        return Promise.resolve(elements);
+        return null;
+    }
+
+
+    public refresh(): void {
+        this._onDidChangeTreeData.fire();
+    }
+
+    public setExpansionState(elementId: string, isExpanded: boolean) {
+        let element = this.elements.find(element => element.include.id === elementId);
+        if (!element) { return; }
+        if (isExpanded) {
+            this.expandedElements.add(element.include.id);
+        }
+        else {
+            this.expandedElements.delete(element.include.id);
+        }
+    }
+
+    public clearExpansionState() {
+        this.expandedElements.clear();
     }
 
     public setIncludeTree(includeTree: IncludeTree | undefined) {
         this.includeTree = includeTree;
+        if (includeTree) {
+            this.elements = this.convertIncludesToIncludeTreeItems(includeTree.flatten());
+        }
         this.refresh();
+    }
+
+    private convertIncludesToIncludeTreeItems(includes: Include[]): IncludeTreeItem[] {
+        let items: IncludeTreeItem[] = [];
+
+        for (const include of includes) {
+            const currentItemState = this.expandedElements.has(include.id) ? vscode.TreeItemCollapsibleState.Expanded : vscode.TreeItemCollapsibleState.Collapsed;
+            const collapsibleState = include.includes.length === 0 ? vscode.TreeItemCollapsibleState.None : currentItemState;
+            const item = new IncludeTreeItem(include, include.getFileName(), {
+                'title': "Open file",
+                'command': "include-tree.open",
+                'tooltip': "Open file",
+                'arguments': [include.fileUri]
+            }, vscode.ThemeIcon.File, include.fileUri, collapsibleState);
+            items.push(item);
+        }
+
+        return items;
     }
 
     private _onDidChangeTreeData: vscode.EventEmitter<IncludeTreeItem | undefined | null | void> = new vscode.EventEmitter<IncludeTreeItem | undefined | null | void>();
     readonly onDidChangeTreeData: vscode.Event<IncludeTreeItem | undefined | null | void> = this._onDidChangeTreeData.event;
-
-    refresh(): void {
-        this._onDidChangeTreeData.fire();
-    }
 }
