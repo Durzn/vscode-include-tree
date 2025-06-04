916ea56e514fa88a545a9fb9725ffb18d710ca3b
Durzn <dev@durzn.com>
2025-06-04 11:57:26 +0200

    IncludeTree can now flatten and set the parent ids

--- src/IncludeTree.ts
+++ src/IncludeTree.ts
@@ -2,5 +2,28 @@ import Include from './Include';
 
 
 export default class IncludeTree {
-    constructor(public rootNodes: Include[]) { }
+    constructor(public rootNodes: Include[]) {
+        for (let node of this.rootNodes) {
+            this.setParent(node);
+        }
+    }
+
+    public flatten(nodes: Include[] = this.rootNodes): Include[] {
+        const includes = nodes.reduce((accumulator, node) => {
+            accumulator.push(node);
+            if (node.includes.length > 0) {
+                accumulator = accumulator.concat(this.flatten(node.includes));
+            }
+            return accumulator;
+        }, [] as Include[]);
+
+        return includes;
+    }
+
+    private setParent(node: Include) {
+        for (let include of node.includes) {
+            include.parent = node;
+            this.setParent(include);
+        }
+    }
 }
